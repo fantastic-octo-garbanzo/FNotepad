89c2ae411d4ede4681a0274708a0093e4817e0a3
Fynn <Fynn_Federer@web.de>
2021-03-24 18:19:01 +0100

    tggg

--- src/FNotepad.java
+++ src/FNotepad.java
@@ -82,7 +82,6 @@ public class FNotepad implements ActionListener {
                             String text = ta.getText();
                             String textTabs = ta.getText();
                             for(char c : textTabs.toCharArray()){
-                                System.out.println(c);
                                 if("\t".equals(""+c)){
                                     letterCount = letterCount + tabSize;
                                 }
