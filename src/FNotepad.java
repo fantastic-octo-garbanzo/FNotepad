8a8ecb244fa63e5a58e1400c2741e2c3279f0be2
fynn <Fynn_Federer@web.de>
2021-06-02 22:03:26 +0200

    Terminal is now functionally on Linux

--- src/FNotepad.java
+++ src/FNotepad.java
@@ -300,7 +300,7 @@ public class FNotepad implements ActionListener {
 /////////////////////////////////////
         else if (cmdText.equals(bundle.getString("commandoopen"))) {
             ProcessBuilder pb = new ProcessBuilder( "C:/WINDOWS/system32/cmd.exe", "/c", "start");
-            ProcessBuilder lt = new ProcessBuilder("bash", "/c", "start");
+            ProcessBuilder lt = new ProcessBuilder("x-terminal-emulator", "/c", "start");
             String os = System.getProperty("os.name").toLowerCase();
             try {
                 if (os.indexOf("win") >= 0) {
