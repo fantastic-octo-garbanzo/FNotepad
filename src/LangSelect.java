2d12602b25995ccefa93df89b4b3afd928837979
fynn <Fynn_Federer@ghse.de>
2021-11-07 18:20:29 +0100

    add Design update

--- src/LangSelect.java
+++ src/LangSelect.java
@@ -3,32 +3,35 @@ package src;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.event.*;
+import java.awt.*;
 import java.net.*;
-import java.util.*; 
+import java.util.*;
 
 /**********/
 
 // Begin of class LangSelect
 public class LangSelect extends JFrame {
-    
+
     public LangSelect(){
-    
+
         String os = System.getProperty("os.name").toLowerCase();
-    
+
+
         try {
             if (os.indexOf("win") >= 0) {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
-                UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             }
         } catch(Exception e) {
             System.err.println("Setting Look and Feel Failed");
         }
-    
+
         setTitle("Language - FNotepad 1.4.2");
         JLabel l = new JLabel("Welcome to FNotepad");
-        l.setBounds(590,50, 100,50);
-        l.setSize(400,100);
+        l.setFont(new Font("Serif", Font.ITALIC, 35));
+        l.setBounds(500,50, 500,100);
+        l.setSize(500,100);
 
         URL iconURL = getClass().getResource("/bin/FNotepad.jpg");
         ImageIcon icon = new ImageIcon(iconURL);
@@ -67,10 +70,10 @@ public class LangSelect extends JFrame {
         setVisible(true);
 
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-    
+
         c.addItemListener(ie -> {
             if(c.getSelectedItem().equals("Deutsch")) {
-                l.setText("Willkommen im FNotepad!");
+                l.setText("Willkommen im FNotepad");
                 setTitle("Sprache - FNotepad 1.4.2");
                 b.setText("Sprache ausw\u00E4hlen");
                 a.setText("Abbrechen");
@@ -79,7 +82,7 @@ public class LangSelect extends JFrame {
                 ch.addItem("Fenstermodus");
             }
             if(c.getSelectedItem().equals("Italiano")) {
-                l.setText("Benvenuto in FNotepad!");
+                l.setText("Benvenuto in FNotepad");
                 setTitle("Lingua - FNotepad 1.4.2");
                 b.setText("seleziona la tua lingua");
                 a.setText("Interrompi");
@@ -88,7 +91,7 @@ public class LangSelect extends JFrame {
                 ch.addItem("finestrato");
             }
             if(c.getSelectedItem().equals("Fran\u00E7ais")) {
-                l.setText("Benvenuto in FNotepad!");
+                l.setText("Benvenuto in FNotepad");
                 setTitle("Langue - FNotepad 1.4.2");
                 b.setText("sélectionnez votre langue");
                 a.setText("Stop");
@@ -97,7 +100,7 @@ public class LangSelect extends JFrame {
                 ch.addItem("Mode fenêtre");
             }
             if(c.getSelectedItem().equals("English")) {
-                l.setText("Welcome to FNotepad!");
+                l.setText("Welcome to FNotepad");
                 setTitle("Language - FNotepad 1.4.2");
                 b.setText("Choose Language");
                 a.setText("Cancel");
@@ -105,7 +108,7 @@ public class LangSelect extends JFrame {
                 ch.addItem("fullscreen");
                 ch.addItem("windowed");
             }
-            });
+        });
 
         b.addActionListener(e -> {
             if(c.getSelectedItem().equals("Deutsch")){
@@ -125,8 +128,8 @@ public class LangSelect extends JFrame {
                 if (ch.getSelectedItem().equals("windowed")) {new FNotepad(false, Locale.ENGLISH);}
             }
             dispose();
-        });  
-    
+        });
+
         a.addActionListener((ActionEvent actionEvent) -> {
             System.exit(0);
         });
